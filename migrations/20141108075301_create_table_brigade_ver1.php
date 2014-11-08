bfdbe42aa4f394620d7506d4a6db05fcbc60a725
Dmitriy Shamrik <shamrik.dmitriy@gmail.com>
2014-11-08 10:55:18 +0300

    Added migrations files
    Added migrations files

--- migrations/20141108075301_create_table_brigade_ver1.php
+++ migrations/20141108075301_create_table_brigade_ver1.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class CreateTableRequestcardVer1 extends AbstractMigration
+class CreateTableBrigadeVer1 extends AbstractMigration
 {
     /**
      * Change Method.
@@ -22,7 +22,19 @@ class CreateTableRequestcardVer1 extends AbstractMigration
      */
     public function up()
     {
-    
+        $sql = <<<SQL
+CREATE TABLE IF NOT EXISTS `brigade` (
+  `id` INT(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `brigadier` VARCHAR(80) NOT NULL,
+  `auto_reg_num` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `auto_reg_num_UNIQUE` (`auto_reg_num` ASC),
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC))
+ENGINE = InnoDB;
+SQL;
+        $this->execute($sql);
     }
 
     /**
