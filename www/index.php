21065f768334b172b24b9ce509dca1b6660083db
Dmitriy Shamrik <shamrik.dmitriy@gmail.com>
2014-11-10 21:55:16 +0600

    Added INK and routing start:)
    Added INK and routing start:)

--- www/index.php
+++ www/index.php
@@ -1,3 +1,16 @@
 <?php
-
-phpinfo();
+$page = 'start';
+$required_page = $_GET['page'];
+if (isset($required_page)) {
+    if(!is_file('controllers/'.$required_page.'php')){
+        ob_start();
+        require_once('controllers/' . $required_page . '.php');
+        $content = ob_get_contents();
+        ob_end_clean();
+    }
+    else{
+        $required_page = $page;
+    }
+    $page = $required_page;
+}
+require_once('views/layout.php');
