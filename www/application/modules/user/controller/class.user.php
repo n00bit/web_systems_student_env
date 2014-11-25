9528eb6c087997688d77a5d9ae59bffaee43782c
Dmitriy Shamrik <shamrik.dmitriy@gmail.com>
2014-11-25 23:45:01 +0600

    repath
    repath

---
+++ www/application/modules/user/controller/class.user.php
@@ -0,0 +1,34 @@
+<?php
+class User{
+    private $name = null;
+    private $surname = null;
+    private $patronymic = null;
+    private $brithdate = null;
+    private $gender = null;
+    private $pasportSeries = null;
+    private $pasportNumber = null;
+    private $pasportDepartment = null;
+    private $pasportAdres = null;
+    private $pasportGetDate = null;
+    private $phoneContact = null;
+    private $loginPhone = null;
+    private $password = null;
+    private $email = null;
+
+
+}
+/*    public function getName();
+    public function getSurname();
+    public function getPatronymic();
+    public function getBrithdate();
+    public function getGender();
+    public function getPasportSeries();
+    public function getPasportNumber();
+    public function getPasportAdress();
+    public function getPasportDate();
+    public function getPasportDepartment();
+    public function getPhoneContact();
+    public function getLoginPhone();
+    public function getEmail();
+    public function getPassword();
+*/
